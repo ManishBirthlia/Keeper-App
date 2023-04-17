f3c6f17f1ddcb7f85d7dd5647ee734a023e0383c
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-04-17 23:23:50 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
+function Header() {
+  return (
+    <header>
+      <h1><LightbulbIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
