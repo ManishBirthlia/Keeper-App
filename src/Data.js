97917286c9de5c05bb28a9f566ec3087d01419b5
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2023-05-04 00:34:08 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,19 @@
+var Data = [
+  {
+    id : 1,
+    title: "This is note title",
+    p: "This is the note content",
+  },
+  {
+    id : 2,
+    title: "This is note title",
+    p: "This is the note content",
+  },
+  {
+    id : 3,
+    title: "This is note title",
+    p: "This is the note content",
+  }
+];
+
+export default Data;
